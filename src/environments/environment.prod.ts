a4d7f4d67ba5016ae8e7763504d80fa956c99da9
unknown <ra314@hotmail.com>
2019-11-12 15:03:17 -0600

    version 1.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig ={
+  apiKey: "AIzaSyCyI1CDHXmHm_5wF47fitzcRNcpGGY23Ok",
+    authDomain: "implementtaapp.firebaseapp.com",
+    databaseURL: "https://implementtaapp.firebaseio.com",
+    projectId: "implementtaapp",
+    storageBucket: "",
+    messagingSenderId: "235670158456",
+    appId: "1:235670158456:web:cd2e01e8711a9117"
+};
